827f813239951694998daa97cb765cf45b12a1fe
rmunozmuno1 <rmunozmuno1@educacion.navarra.es>
2024-10-25 20:36:06 +0200

    A

---
+++ Ejercicio09.py
@@ -0,0 +1,9 @@
+
+contraseña_correcta = 'contraseña'
+contraseña_introducida = ''
+
+while contraseña_introducida != contraseña_correcta:
+    contraseña_introducida = input("Introduce la contraseña: ")
+    if contraseña_introducida != contraseña_correcta:
+        print("Contraseña incorrecta, inténtalo de nuevo.")
+print ('contraseña correcta')
