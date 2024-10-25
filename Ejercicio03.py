4b8aeb3ae0fdbe82d98a52a11dee38737de330b0
rmunozmuno1 <rmunozmuno1@educacion.navarra.es>
2024-10-25 09:10:42 +0200

    Ejercicio04

---
+++ Ejercicio03.py
@@ -0,0 +1,7 @@
+###Escribir un programa que pida al usuario un número entero y muestre por pantalla si es par o impar.
+Numero = int(input('Introduzca un numero entero'))
+if (Numero) % 2 == 0 :
+    print ('par')
+elif (Numero) % 2 != 0 :
+    print ('impar')
+ 
